5d036fd7d08a4e9538667c94909185113f8d3a82
Rosset <theo.rosset@outlook.fr>
2023-12-07 08:43:59 +0100

    fix(react): add react in peer Depencies and add external in rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import postcss from "rollup-plugin-postcss"
 import dts from "rollup-plugin-dts";
+import external from 'rollup-plugin-peer-deps-external';
 
 import packageJson  from"./package.json" assert { type: "json" };
 
@@ -22,6 +23,7 @@ export default [
       },
     ],
     plugins: [
+      external(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
@@ -32,6 +34,6 @@ export default [
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
-    external: [/\.css$/]
+    external: [/\.css$/, "react", "react-dom"]
   },
 ];
